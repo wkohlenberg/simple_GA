948ec1820d49c8ec2ec68eb000e088ead4c3bf37
wkohlenberg <walterkohlenberg@gmail.com>
2017-04-17 23:09:25 +0200

    Update return datatype function 'createRandomPopulation'.

--- src/population.h
+++ src/population.h
@@ -5,7 +5,7 @@ class POPULATION
 {
 public:
   int **initializePopulation(int individuals, int genes, bool FILL_ZERO = false);
-  int **createRandomPopulation(int **population, int individuals, int genes);
+  int createRandomPopulation(int **population, int individuals, int genes);
 };
 
 #endif
