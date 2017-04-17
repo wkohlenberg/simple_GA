5a261c44c2886f3536a193225f0135028288a0e0
wkohlenberg <walterkohlenberg@gmail.com>
2017-04-17 22:45:52 +0200

    Added functions for population in  class 'POPULATION'

---
+++ src/population.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "population.h"
+
+int **POPULATION::initializePopulation(int individuals, int genes, bool FILL_ZERO)
+{
+  int **population;
+
+  if (!FILL_ZERO)
+  {
+    population = (int **) malloc(individuals * sizeof(int *));
+    for (int i = 0; i < individuals; i++)
+      population[i] = (int *) malloc(genes * sizeof(int));
+  }
+  else
+  {
+    population = (int **) calloc(individuals, sizeof(int *));
+    for (int i = 0; i < individuals; i++)
+      population[i] = (int *) calloc(genes, sizeof(int));
+  }
+
+
+  return population;
+}
+
+int **POPULATION::createRandomPopulation(int **population, int individuals, int genes)
+{
+  // Fill the 2d array with random genes
+  for (int pop = 0; pop < individuals; pop++)
+  {
+    for (int gene = 0; gene < genes; gene++)
+    {
+      population[pop][gene] = rand() % 2;
+    }
+  }
+  return 0;
+}
