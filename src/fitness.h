2d3b2dd75f479cd0744c7d0adac6357842a958d9
wkohlenberg <walterkohlenberg@gmail.com>
2017-04-17 17:45:51 +0200

    Changed argument of function 'getSolutionLength'

--- src/fitness.h
+++ src/fitness.h
@@ -8,12 +8,13 @@ class FITNESS
 public:
   int setSolution(std::string solution);
   std::string getSolution();
-  int getSolutionLength(std::string solution);
+  int getSolutionLength();
   int getFitness(int *individual);
   int getFittest(int **pop, int popSize);
 
 private:
   std::string fitnessSolution;
+  int geneSize;
 };
 
 #endif
