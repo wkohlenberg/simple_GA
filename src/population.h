7eac7c37d14777e75591b09bfc6cb8d3719d7df7
wkohlenberg <walterkohlenberg@gmail.com>
2017-05-02 23:40:42 +0200

    Added a variable 'populationSize'. Usefull for dynamic population instead of a default value.

--- src/population.h
+++ src/population.h
@@ -1,9 +1,13 @@
 #ifndef POPULATION_H
 #define POPULATION_H
 
+#define POPULATION_SIZE     50
+
 class POPULATION
 {
 public:
+  int populationSize;
+
   int **initializePopulation(int individuals, int genes, bool FILL_ZERO = false);
   int **createRandomPopulation(int **population, int individuals, int genes);
 };
