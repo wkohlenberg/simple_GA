edcf23c804724ff047e5dd5e926e12f64c973895
wkohlenberg <walterkohlenberg@gmail.com>
2017-05-02 23:41:45 +0200

    Update the default population size to the variable 'populationSize'.

--- src/evolution.cpp
+++ src/evolution.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "evolution.h"
+#include "population.h"
 
 int EVOLUTION::crossover(int **newPop,
               int **pop,
@@ -30,7 +31,7 @@ int EVOLUTION::crossover(int **newPop,
 
 // Function tournament selection
 // Chooses 5 random individuals and selects the fittest
-int EVOLUTION::tournamentSelection(int **population, FITNESS Fitness)
+int EVOLUTION::tournamentSelection(int **population, int populationSize, FITNESS Fitness)
 {
   int fittest = 0;
   int geneSize = 0;
@@ -55,7 +56,7 @@ int EVOLUTION::tournamentSelection(int **population, FITNESS Fitness)
   for (int i = 0; i < TOURNAMENT_SIZE; i++)
   {
     double random = ((double)rand() / RAND_MAX);
-    int randomId = (int)(random * POPULATION_SIZE);
+    int randomId = (int)(random * populationSize);
     individu[i] = randomId;
     for (int j = 0; j < geneSize; j++)
     {
@@ -72,7 +73,7 @@ int EVOLUTION::tournamentSelection(int **population, FITNESS Fitness)
 // The fittest of the population is copied to the new generation
 // The others individuals are created by parents
 // Mutate some random genes of the individuals
-int EVOLUTION::evolve(int **population, FITNESS Fitness)
+int EVOLUTION::evolve(int **population, int populationSize, FITNESS Fitness)
 {
   int geneSize = 0;
 
@@ -81,26 +82,26 @@ int EVOLUTION::evolve(int **population, FITNESS Fitness)
 
   // Initialize the new population
   POPULATION Population;
-  int **newPop = Population.initializePopulation(POPULATION_SIZE, geneSize);
+  int **newPop = Population.initializePopulation(populationSize, geneSize);
 
 
   // Copy the fittest to the first position
-  int fittestIndividual = Fitness.getFittest(population, POPULATION_SIZE);
+  int fittestIndividual = Fitness.getFittest(population, populationSize);
   for (int i = 0; i < geneSize; i++)
   {
     newPop[0][i] = population[fittestIndividual][i];
   }
 
   // Select parents and let them pair
-  for (int index = 0; index < POPULATION_SIZE; index++)
+  for (int index = 0; index < populationSize; index++)
   {
-    int firstTournament = tournamentSelection(population, Fitness);
-    int secondTournament = tournamentSelection(population, Fitness);
+    int firstTournament = tournamentSelection(population, populationSize, Fitness);
+    int secondTournament = tournamentSelection(population, populationSize, Fitness);
     crossover(newPop, population, index, firstTournament, secondTournament, Fitness);
   }
 
   // Mutate genes
-  for (int i = 0; i < POPULATION_SIZE; i++)
+  for (int i = 0; i < populationSize; i++)
   {
     for (int j = 0; j < geneSize; j++)
     {
@@ -113,7 +114,7 @@ int EVOLUTION::evolve(int **population, FITNESS Fitness)
   }
 
   // Copy the evolved population to the population
-  for (int i = 0; i < POPULATION_SIZE; i++)
+  for (int i = 0; i < populationSize; i++)
   {
     for (int j = 0; j < geneSize; j++)
     {
