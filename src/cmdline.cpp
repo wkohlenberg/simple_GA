1cc4be8bf4a706be981fdb4e605ba8644801a13b
wkohlenberg <walterkohlenberg@gmail.com>
2017-06-19 16:33:32 +0200

    Add help page for in terminal/cmdline

--- src/cmdline.cpp
+++ src/cmdline.cpp
@@ -12,7 +12,19 @@ CMDLINE::CMDLINE(int argC, char **argV)
 
 int CMDLINE::help()
 {
-  std::cout << "Help! (coming soon)" << std::endl;
+  std::cout << "This is a simple implementation of the genetic algorithm." << std::endl;
+               version();
+  std::cout << std::endl;
+  std::cout << "Usage: main [OPTIONS]" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Options: " << std::endl;
+  std::cout << "  --population, -p\tSelect the size of the population [default: 50]." << std::endl;
+  std::cout << "  --genes, -g\t\tSelect the gene size of the individu [default: 64]" << std::endl;
+  std::cout << "  --version, -v\t\tDisplay the software version" << std::endl;
+  std::cout << "  --help, -h, -?\tShow help" << std::endl;
+
+  exit(0);
+
   return 0;
 }
 
@@ -38,7 +50,7 @@ int CMDLINE::parseCommandLine(cmdoptions_t *CMDoptions)
         help();
     }
     else if ((strcmp(option, "--version") == 0)
-          || (strcmp(option, "-h") == 0))
+          || (strcmp(option, "-v") == 0))
     {
       version();
     }
